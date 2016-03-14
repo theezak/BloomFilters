a1598f412f145c151710d8af36a3762f61379ca0
theezak <mrentier@cox.net>
2016-03-13 22:12:07 -0500

    Add high utlization keyvalue pair configuration

---
+++ TBag.BloomFilters/HighUtilizationKeyValuePairBloomFilterConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace TBag.BloomFilters
+{
+    using System.Collections.Generic;
+   
+    /// <summary>
+    /// A default Bloom filter configuration for identifiers with a precomputed value hash. Well suited for Bloom filters that are utilized according to their capacity.
+    /// </summary>
+    public abstract class HighUtilizationValuePairBloomFilterConfiguration :
+        HighUtilizationBloomFilterConfigurationBase<KeyValuePair<long,int>>
+    {
+      
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="createValueFilter"></param>
+        protected HighUtilizationValuePairBloomFilterConfiguration(bool createValueFilter = true) : 
+            base(createValueFilter)
+        {
+        }
+
+        protected override long GetIdImpl(KeyValuePair<long, int> entity)
+        {
+            return entity.Key;
+        }
+
+        protected override int GetEntityHashImpl(KeyValuePair<long, int> entity)
+        {
+            return entity.Value;
+        }
+    }
+}
+
